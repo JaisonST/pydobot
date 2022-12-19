bdd67dc45aae21aad7b2e65ae47ec3560591e9a7
JaisonST <jaisonmanu@gmail.com>
2022-12-19 16:47:53 +0400

    sensor and conveyor belt functions

--- pydobot/dobot.py
+++ pydobot/dobot.py
@@ -3,12 +3,17 @@ import struct
 import time
 import threading
 import warnings
+import math
 
 from .message import Message
 from .enums import PTPMode
 from .enums.CommunicationProtocolIDs import CommunicationProtocolIDs
 from .enums.ControlValues import ControlValues
 
+PORT_GP1 = 0x00
+PORT_GP2 = 0x01
+PORT_GP4 = 0x02
+PORT_GP5 = 0x03
 
 class Dobot:
 
@@ -330,3 +335,55 @@ class Dobot:
         j3 = struct.unpack_from('f', response.params, 24)[0]
         j4 = struct.unpack_from('f', response.params, 28)[0]
         return x, y, z, r, j1, j2, j3, j4
+
+    def set_ir(self, enable=True, port=PORT_GP4):
+        msg = Message()
+        msg.id = 138
+        msg.ctrl = 0x02
+        msg.params = bytearray([])
+        msg.params.extend(bytearray([int(enable)]))
+        msg.params.extend(bytearray([port]))
+        return self._extract_cmd_index(self._send_command(msg))
+
+    def get_ir(self, port=PORT_GP4):
+        msg = Message()
+        msg.id = 138
+        msg.ctrl = 0x00
+        msg.params = bytearray([])
+        msg.params.extend(bytearray([port]))
+        response = self._send_command(msg)
+        state = struct.unpack_from('?', response.params, 0)[0]
+        return state
+    
+    def set_color(self, enable=True, port=PORT_GP2):
+        msg = Message()
+        msg.id = 137
+        msg.ctrl = 0x03
+        msg.params = bytearray([])
+        msg.params.extend(bytearray([int(enable)]))
+        msg.params.extend(bytearray([port]))
+        return self._extract_cmd_index(self._send_command(msg))
+
+    def get_color(self, port=PORT_GP2):
+        msg = Message()
+        msg.id = 137
+        msg.ctrl = 0x00
+        msg.params = bytearray([])
+        msg.params.extend(bytearray([port]))
+        response = self._send_command(msg)
+        r = struct.unpack_from('?', response.params, 0)[0]
+        g = struct.unpack_from('?', response.params, 1)[0]
+        b = struct.unpack_from('?', response.params, 2)[0]
+        return [r, g, b]
+
+    def conveyor_belt_distance(self, speed_mm_per_sec, distance_mm, direction=1, interface=0):
+        if speed_mm_per_sec > 100:
+            print("Speed must be <= 100 mm/s")
+            return
+
+        MM_PER_REV = 34 * math.pi  # Seems to actually be closer to 36mm when measured but 34 works better
+        STEP_ANGLE_DEG = 1.8
+        STEPS_PER_REV = 360.0 / STEP_ANGLE_DEG * 10.0 * 16.0 / 2.0  # Spec sheet says that it can do 1.8deg increments, no idea what the 10 * 16 / 2 fudge factor is....
+        distance_steps = distance_mm / MM_PER_REV * STEPS_PER_REV
+        speed_steps_per_sec = speed_mm_per_sec / MM_PER_REV * STEPS_PER_REV * direction
+        return self._extract_cmd_index(self._set_stepper_motor_distance(int(speed_steps_per_sec), int(distance_steps), interface))
